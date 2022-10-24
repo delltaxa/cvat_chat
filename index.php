618d5cc7b4e822478383b19549100cc0e2e7be62
delltaxa <114283067+delltaxa@users.noreply.github.com>
2022-10-24 16:04:39 +0200

    Add files via upload
    Fixed chromium bug by changing the login system

--- index.php
+++ index.php
@@ -15,7 +15,6 @@ session_start();
     <script>
         var opkey = "";
 
-
         function set_user(name) {
             document.getElementById('uname').value = name;
         }
@@ -54,77 +53,83 @@ session_start();
             return new Promise(resolve => setTimeout(resolve, ms));
         }
 
-        function verf(a) {
-            $.post("verify_key.php",
-                   {kee: a},
-                   function(response){
-                        if (response == 1) {
-                            return true;
-                        }
-                        else {
-                            var askAgain = prompt('key', "xxxxxxxxx");
-                            verf(askAgain);
-                        }
-                   }
-            );
-        }
 
-        window.onload = function() {           
-            const inkey = prompt("key", "xxxxxxxxx");
+        function askagin() {
+            let inkey = document.getElementById('keebox').value;
             opkey = inkey;
             $.post("verify_key.php",
                    {kee: inkey},
                    function(response){
                         if (response == 1) {
-                            // correct
+                            $("#messages").load("gmsg.php?pawd="+opkey);
+                            if (!document.cookie.includes("cvat")) {
+                                let user = "cvat" + Math.floor(Math.random() * (9999 - 1000) + 1000);
+                                set_user(user);
+                                createCookie("usdn", user, 1)
+                            }
+                            else {
+                                set_user(getCookie("usdn"));
+                            }
+                        
+                            $(document).ready(function () {
+                            $('#pbost').on('submit', function(e) {
+                                e.preventDefault();
+                                $.ajax({
+                                    url : $(this).attr('action'),
+                                    type: "POST",
+                                    data: $(this).serialize()
+                                });
+                                document.getElementById('message').value = "";
+                            });
+                            });
+                        
+                            var auto_refresh = setInterval(
+                                (function () {
+                                    $("#messages").load("gmsg.php?pawd="+opkey);
+                            }), 1000);
+
+                            
+                            document.getElementById('inkuy').value = inkey;
+                            document.getElementById('ffm').hidden = true;
+                            document.getElementById('fff').hidden = true;
+                            document.getElementById('pbost').hidden = false;
                         }
                         else {
-                            window.location = "/";
+                            document.getElementById('loginmsg').style.color = "red";
+                            document.getElementById('loginmsg').innerText = "";
+                            setTimeout(() => { document.getElementById('loginmsg').innerText = "Incorrect Key!"; }, 200);
                         }
                    }
             );
-
-            $("#messages").load("gmsg.php?pawd="+opkey);
-            if (!document.cookie.includes("cvat")) {
-                let user = "cvat" + Math.floor(Math.random() * (9999 - 1000) + 1000);
-                set_user(user);
-                createCookie("usdn", user, 1)
-            }
-            else {
-                set_user(getCookie("usdn"));
-            }
-
-            
         }
 
-        $(document).ready(function () {
-            $('#pbost').on('submit', function(e) {
-                e.preventDefault();
-                $.ajax({
-                    url : $(this).attr('action'),
-                    type: "POST",
-                    data: $(this).serialize()
-                });
-                document.getElementById('message').value = "";
-            });
-        });
-
-        var auto_refresh = setInterval(
-            (function () {
-                $("#messages").load("gmsg.php?pawd="+opkey);
-        }), 1000);
+        window.onload = function() {      
+            if (window.location != "?#messages") {
+                window.location = "?#messages"
+            }
+        }     
         
     </script>
 </head>
 <body>
+    <form class="vertical-center" onsubmit="askagin();" id="ffm" style="display:flex; justify-content: center;">
+        <div id="fff" style="width: 260px; height: 130px; background-color: white; float: left; padding: 30px; border-radius: 5px;">
+            <input type="password" style="height: 40px; width: 240px; font-size: 20px;" id="keebox">
+            <br/>
+            <p id="loginmsg" style="font-size: 16px; font-weight: bolder">Enter The Key</p>
+            <input type="submit" style="width: 250px; height: 45px; text-align: center;">
+        </div>
+    </form>
+
     <div id="messages">
     </div>
-    <form action="post.php" id="pbost" method="POST">
+    <form action="post.php" id="pbost" method="POST" hidden>
         <input hidden type="text" id="uname" name="name" <?php if ($_SESSION['name'] !== null) {echo 'value=' . $_SESSION['name'];} ?> />
+        <input hidden type="text" name="inkay" id="inkuy">
         <p></p>
 
         <div style="text-align: center;">
-            <input type="text" id="message" class="msag" name="message" style="border-radius: 5px 0px 0px 5px;" autofocus />
+            <input type="text" id="message" class="msag" name="message" style="border-radius: 5px 0px 0px 5px;" autofocus /> 
             <input type="submit" value="Send" style="border-radius: 0px 5px 5px 0px;" />
         </div>
 
--- post.php
+++ post.php
@@ -5,6 +5,7 @@
     
     session_start();
 
+    
     function scrub($input) {
         $splitted = explode('<', $input);
         $splitted = implode('&lt;', $splitted);
@@ -13,56 +14,63 @@
         return $splitted;
     }
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        if ($_POST['type'] == "nojs") {
-            $data['name'] = $_POST['name'];
-            $data['message'] = $_POST['message'];
-
-            header('Location: nojs.php#message');
-        } else {
-        
-            $data = json_decode(file_get_contents("php://input"), false);
-            print_r($data);
-        }
-    }
-
-    $data = (array) $data;
-
-    date_default_timezone_set("UTC");
-    
-    $chat = fopen('chat.txt', 'a') or die('Error upon opening file');
-
-    $name = 'anonymous';
-
-    if ($_POST['name'] !== '') {
-        $name = $data['name'];
-    }
-
-    $message = scrub($data['message']);
-    
-    $name = scrub($name);
-    $name = explode(' ', $name);
-    $name = implode('_', $name);
-
-    $_SESSION['name'] = $name;
-
-    $text = '<name>' . $name . '</name>' . '<message>' . $message . '</message>  <date>' . date("Y-m-d") . ' ' . date("H:i:s") . "</date>\n";
-
     include('config.php');
     include('endecr.php');
 
-    $text_paus = encrypt_decrypt("encrypt", $text, $passw);
-    $text = $text_paus . "\n";
+    $inps = $_POST['inkay'];
 
-    if ($message !== '') {
-        $status = 'Success!';
-        fwrite($chat, $text);
+    if ($inps == $passw) {
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if ($_POST['type'] == "nojs") {
+                $data['name'] = $_POST['name'];
+                $data['message'] = $_POST['message'];
+
+                header('Location: index.php#message');
+            } else {
+            
+                $data = json_decode(file_get_contents("php://input"), false);
+                print_r($data);
+            }
+        }
+
+        $data = (array) $data;
+
+        date_default_timezone_set("UTC");
+
+        $chat = fopen('chat.txt', 'a') or die('Error upon opening file');
+
+        $name = 'anonymous';
+
+        if ($_POST['name'] !== '') {
+            $name = $data['name'];
+        }
+
+        $message = scrub($data['message']);
+
+        $name = scrub($name);
+        $name = explode(' ', $name);
+        $name = implode('_', $name);
+
+        $_SESSION['name'] = $name;
+
+
+        $text = '<name>' . $name . '</name>' . '<message>' . $message . '</message>  <date>' . date("Y-m-d") . ' ' . date("H:i:s") . "</date>\n";
+
+        $text_paus = encrypt_decrypt("encrypt", $text, $passw);
+        $text = $text_paus . "\n";
+
+        if ($message !== '') {
+            $status = 'Success!';
+            fwrite($chat, $text);
+        }
+        else {
+            $status = 'Error. No message';
+        }
+
+        echo $status;
+
+        fclose($chat);
+
     }
-    else {
-        $status = 'Error. No message';
-    }
-
-    echo $status;
-
-    fclose($chat);
 ?>
